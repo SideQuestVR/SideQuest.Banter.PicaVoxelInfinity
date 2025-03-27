0d1149ec752c9147f98f325aa7e63bcf2eecf00e
Gareth Williams <gareth@garethw.net>
2025-03-27 18:07:08 +0000

    Fixed terrain height

--- Packages/com.garethiw.picavoxelinfinity/Scripts/Runtime/TerrainGenerator.cs
+++ Packages/com.garethiw.picavoxelinfinity/Scripts/Runtime/TerrainGenerator.cs
@@ -24,8 +24,6 @@ namespace PicaVoxel
         
         private FastNoiseLite _noise;
 
-        private int _seaLevel;
-        
         public bool GenerateVoxel(int x, int y, int z, ref Voxel voxel)
         {
             if (_noise == null)
@@ -39,8 +37,6 @@ namespace PicaVoxel
                 _noise.SetFractalGain(0.5f);
                 _noise.SetFractalOctaves(4);
                 _noise.SetFractalLacunarity(2.5f);
-
-                _seaLevel = (HeightMax - HeightMin) - HeightMin;
             }
 
             voxel.Color = Color.white;
@@ -48,10 +44,10 @@ namespace PicaVoxel
             val = (val + 1) * 0.5f;
             // if(Random.Range(0,100)==0)
             //     Debug.Log(val);
-            val *= (HeightMax - HeightMin);
+            val *= (HeightMax-HeightMin);
             //Debug.Log(val);
-            voxel.Active = y < _seaLevel+ val;
-            voxel.Value = y<=BedrockHeight? (byte)4: ((y+1) < _seaLevel+ val) ? (byte)1 : (byte)0;
+            voxel.Active = y < HeightMin + val;
+            voxel.Value = y<=BedrockHeight? (byte)4: ((y+1) < HeightMin +val) ? (byte)1 : (byte)0;
 
             return true;
         }
